bbe7bafdc7496eaf4ae83615dd950788db92ec2a
Andrew Lunde <andrew.lunde@sap.com>
2021-04-15 17:36:08 -0400

    init

---
+++ start_theta.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# run netcat listening on 1234 to simulate a TCP/IP based server
+# exec nc -l 1234 &>/dev/null &
+#
+#
+echo "Sleepy..."
+sleep 10
+# Start the MySQL Server
+#
+echo "THETA Private Network Start."
